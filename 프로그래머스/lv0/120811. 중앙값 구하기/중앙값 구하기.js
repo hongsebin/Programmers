69b168b3225f0a146d2b76ab015daddcbf96761b
홍세빈 <116236833+hongsebin@users.noreply.github.com>
2023-05-15 14:48:17 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,10 @@
 function solution(arr) {
+    
+    // 오름차순 정렬 : arr.sort((a,b)=>a-b);
+    // 내림차순 정렬 : arr.sort((a,b)=>b-a);
+    
     arr.sort((a,b) => a-b);
+    
     var val = parseInt(arr.length/2);
     return arr[val];
 }
