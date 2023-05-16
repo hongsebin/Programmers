7423e56b55a7e371ebccf768693ee54d1d852f55
홍세빈 <116236833+hongsebin@users.noreply.github.com>
2023-05-16 09:46:57 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    
-    return n % 7 === 0 ? Math.floor(n/7) : Math.floor(n/7)+1;
+    return Math.ceil(n / 7)
+    // return n % 7 === 0 ? Math.floor(n/7) : Math.floor(n/7)+1;
 }
