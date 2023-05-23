1d3855676017fc2ab9a489a99c6abafbef226ecf
홍세빈 <116236833+hongsebin@users.noreply.github.com>
2023-05-23 19:04:22 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,7 @@
 function solution(hp) {
-  const first = Math.floor(hp / 5);
-  const second = Math.floor((hp - first * 5) / 3);
-  const third = hp - first * 5 - second * 3;
-  return first + second + third;
+    const a = Math.floor(hp/5);
+    const b = Math.floor((hp-(5*a))/3);
+    const c = hp-((5*a)+(3*b));
+    
+    return a+b+c;
 }
